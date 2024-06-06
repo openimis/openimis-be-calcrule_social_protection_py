9c36735b7de6fbbea5766d99864df69544dee632
sniedzielski <sniedzielski@soldevelo.com>
2024-06-06 13:14:09 +0200

    CM-921: fixed issue with head not taken as benefit recipient

--- calcrule_social_protection/converters/group_beneficiary/group_to_benefit.py
+++ calcrule_social_protection/converters/group_beneficiary/group_to_benefit.py
@@ -7,17 +7,23 @@ class GroupToBenefitConverter(BuilderToBenefitConverter):
     def to_benefit_obj(cls, entity, amount, payment_plan, payment_cycle):
         group_head = GroupIndividual.objects.filter(
             group_id=entity.group.id,
-            role=GroupIndividual.RecipientType.PRIMARY.value,
+            role=GroupIndividual.Role.HEAD.value,
             is_deleted=False
         ).first()
         if group_head:
             return super().to_benefit_obj(group_head, amount, payment_plan, payment_cycle)
-        else:
-            group_head = GroupIndividual.objects.filter(
-                group_id=entity.group.id,
-                is_deleted=False
-            ).first()
-            return super().to_benefit_obj(group_head, amount, payment_plan, payment_cycle)
+        group_primary = GroupIndividual.objects.filter(
+            group_id=entity.group.id,
+            role=GroupIndividual.RecipientType.PRIMARY.value,
+            is_deleted=False
+        ).first()
+        if group_primary:
+            return super().to_benefit_obj(group_primary, amount, payment_plan, payment_cycle)
+        group_head = GroupIndividual.objects.filter(
+            group_id=entity.group.id,
+            is_deleted=False
+        ).first()
+        return super().to_benefit_obj(group_head, amount, payment_plan, payment_cycle)
 
     @classmethod
     def _build_individual(cls, benefit, entity):
